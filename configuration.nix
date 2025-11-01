ad45645dbc24676f2bdc38790c290a39fe683252
Sam Bartley <solojessy.rs@gmail.com>
2025-11-01 18:11:16 +0000

    added missing 'in'

--- configuration.nix
+++ configuration.nix
@@ -5,6 +5,7 @@
 { config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz;
+in
 {
   imports =
     [ # Include the results of the hardware scan.
