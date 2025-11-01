ab6ee210b771b5881b0a71c98511864d3de1fa4e
Sam Bartley <solojessy.rs@gmail.com>
2025-11-01 20:20:51 +0000

    fixed incorrectly named input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  inputs.homemanager = {
+  inputs.home-manager = {
     url = "github:nix-community/home-manager/release-25.05";
     inputs.nixpkgs.follows = "nixpkgs";
   };
