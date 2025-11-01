0731f7b6cad9dfe690248125dae945b198f3f2f9
Sam Bartley <solojessy.rs@gmail.com>
2025-11-01 20:25:08 +0000

    fix git config settings

--- home.nix
+++ home.nix
@@ -20,12 +20,12 @@
 
   programs.git = {
     enable = true;
-    # userName = "Sam Bartley";
-    # userEmail = "SoloJessy.rs@gmail.com";
-    settings = {
-      user.name = "Sam Bartley";
-      user.email = "SoloJessy.rs@gmail.com";
-    };
+    userName = "Sam Bartley";
+    userEmail = "SoloJessy.rs@gmail.com";
+    # settings = {
+    #   user.name = "Sam Bartley";
+    #   user.email = "SoloJessy.rs@gmail.com";
+    # };
   };
 
   programs.starship = {
