cddc0b5e61f425bffc1ca0336d9542461127604a
Sam Bartley <solojessy.rs@gmail.com>
2025-11-02 12:31:13 +0000

    add gaming software

--- steam.nix
+++ steam.nix
@@ -7,4 +7,16 @@
   };
 
   # services.xserver.videoDrivers = [ "amdgpu" ];
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    mangohud
+    protonup-qt
+    lutris
+    bottles
+    heroic
+  ];
 }
