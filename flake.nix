998a578515190d59182f5149744aa70bd9e489e6
Sam Bartley <solojessy.rs@gmail.com>
2025-11-09 23:20:55 +0000

    use rust-analyzer-nightly

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
               "rustc"
               "rustfmt"
             ])
-            pkgs.rust-analyzer
+            pkgs.rust-analyzer-nightly
             pkgs.gcc
           ];
         })
