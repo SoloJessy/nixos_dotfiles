0cf2551ca72321203cc506389955e2d29c3d1000
Sam Bartley <solojessy.rs@gmail.com>
2025-11-11 19:56:53 +0000

    add space in git_metrics

--- home.nix
+++ home.nix
@@ -158,7 +158,7 @@
       git_metrics.disabled = false;
       git_metrics.only_nonzero_diffs = false;
       git_metrics.format =
-        "\\[([+$added]($added_style))\\]\\[([-$deleted]($deleted_style))\\] ";
+        "\\[([+$added]($added_style))\\] \\[([-$deleted]($deleted_style))\\] ";
       git_state.format =
         "\\[[$state ($progress_current/$progress_total)]($style)\\] ";
       # git_status.format = "\\[[$all_status$ahead_behind]($style)\\] ";
