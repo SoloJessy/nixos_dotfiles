7724af605c2d177a180715fbc94548e26f13cf0a
Sam Bartley <solojessy.rs@gmail.com>
2025-11-16 15:32:19 +0000

    remove unused named outputs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, fenix, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, fenix, ... }: {
     nixosConfigurations.nixos-old = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
