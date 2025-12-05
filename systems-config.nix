f9f1220f1a2468e02b92716e598095ec01d72192
Sam Bartley <solojessy.rs@gmail.com>
2025-12-05 18:17:51 +0000

    remove cosmic-edit

--- systems-config.nix
+++ systems-config.nix
@@ -57,6 +57,9 @@
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
+  environment.cosmic.excludePackages = with pkgs; [
+    cosmic-edit
+  ];
 
   # Configure console keymap
   console.keyMap = "uk";
