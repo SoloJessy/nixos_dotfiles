2b311cbf80da9e91f6495fc658cdc1a2982024ac
Sam Bartley <solojessy.rs@gmail.com>
2025-12-20 21:17:51 +0000

    see if this works

--- firefox.nix
+++ firefox.nix
@@ -52,8 +52,8 @@ in
             installation_mode = "force_installed";
           };
           # Bitwarden
-          "446900e4-71c2-419f-a6a7-df9c091e268b" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/446900e4-71c2-419f-a6a7-df9c091e268b/latest.xpi";
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/{446900e4-71c2-419f-a6a7-df9c091e268b}/latest.xpi";
             installation_mode = "forec_installed";
           };
           # Privicy Badger
