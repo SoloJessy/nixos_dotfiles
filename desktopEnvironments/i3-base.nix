3052b560b7a63aabc2a85798a8109a95e7d11230
Sam Bartley <SoloJessy.rs@gmail.com>
2025-12-27 23:05:51 +0000

    more fleshed out i3 config

--- desktopEnvironments/i3-base.nix
+++ desktopEnvironments/i3-base.nix
@@ -10,7 +10,21 @@
         dmenu
         i3status
         i3lock
+        i3a
       ];
+    };
+  };
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Experimental = true;
+        FastConnectable = true;
+      };
+      Policy = {
+        AutoEnable = true;
+      };
     };
   };
   services.blueman.enable = true;
--- desktopEnvironments/i3-home.nix
+++ desktopEnvironments/i3-home.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   xsession.windowManager.i3 = {
     enable = true;
@@ -6,15 +6,73 @@
       modifier = "mod4";
       terminal = "alacritty";
       menu = "${pkgs.dmenu}/bin/dmenu_run -nf \"#bbbbbb\" -nb \"#222222\" -sb \"#613583\" -sf \"#eeeeee\" -fn \"monospace:size=10\" -p \"dmenu\"";
+      defaultWorkspace = "1";
       focus = {
-        followMouse = true;
+        followMouse = false;
         mouseWarping = true;
       };
+      startup = [
+        {
+          always = true;
+          command = "i3a-master-stack --stack=i3";
+        }
+      ];
+      assigns = {
+        "9: games" = [ { class = "(?i)steam"; } ];
+      };
+
+      keybindings =
+        let
+          mod = config.xsession.windowManager.i3.config.modifier;
+          term = config.xsession.windowManager.i3.config.terminal;
+          menu = config.xsession.windowManager.i3.config.menu;
+          browser = "firefox";
+        in
+        {
+          # Workspaces
+          "${mod}+1" = "workspace number 1";
+          "${mod}+2" = "workspace number 2";
+          "${mod}+3" = "workspace number 3";
+          "${mod}+4" = "workspace number 4";
+          "${mod}+5" = "workspace number 5";
+          "${mod}+6" = "workspace number 6";
+          "${mod}+7" = "workspace number 7";
+          "${mod}+8" = "workspace number 8";
+          "${mod}+9" = "workspace number 9";
+          # Window Management
+          "${mod}+h" = "focus left";
+          "${mod}+j" = "focus down";
+          "${mod}+k" = "focus up";
+          "${mod}+l" = "focus right";
+          "${mod}+Shift+h" = "move left";
+          "${mod}+Shift+j" = "move down";
+          "${mod}+Shift+k" = "move up";
+          "${mod}+Shift+l" = "move right";
+
+          "${mod}+Up" = "exec i3a-resize-compass up 20px";
+          "${mod}+Down" = "exec i3a-resize-compass down 20px";
+          "${mod}+Right" = "exec i3a-resize-compass right 20px";
+          "${mod}+Left" = "exec i3a-resize-compass left 20px";
+
+          # i3 commands
+          "${mod}+q" = "kill";
+          "${mod}+Shift+r" = "restart";
+          # exec
+          "${mod}+Return" = "exec ${term}";
+          "${mod}+p" = "exec ${menu}";
+          "${mod}+b" = "exec ${browser}";
+        };
+
       bars = [
         {
           statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
           mode = "dock";
           position = "top";
+          fonts = {
+            names = [ "DroidSansM Nerd Font" ];
+            style = "Normal Semi-Condensed";
+            size = 8.0;
+          };
           colors = {
             background = "#241f31";
             statusline = "#ffffff";
@@ -57,10 +115,10 @@
         };
         focusedInactive = {
           border = "#333333";
-          background = "#c5f676a";
-          text = "#ffffff";
-          indicator = "#484e50";
-          childBorder = "#5f676a";
+          background = "#222222";
+          text = "#888888";
+          indicator = "#292d2e";
+          childBorder = "#222222";
         };
         unfocused = {
           border = "#333333";
@@ -89,41 +147,45 @@
   };
   programs.i3status-rust = {
     enable = true;
-    bars.top.settings = {
-      theme = {
-        theme = "plain";
-        overrides = {
-          idle_bg = "#241f31";
-          info_bg = "#241f31";
-          good_bg = "#241f31";
-          warning_bg = "#241f31";
-          critical_bg = "#241f31";
-          separator_bg = "#241f31";
+    bars.top = {
+
+      icons = "material-nf";
+      settings = {
+        theme = {
+          theme = "plain";
+          overrides = {
+            idle_bg = "#241f31";
+            info_bg = "#241f31";
+            good_bg = "#241f31";
+            warning_bg = "#241f31";
+            critical_bg = "#241f31";
+            separator_bg = "#241f31";
+          };
         };
       };
+      blocks = [
+        {
+          block = "time";
+        }
+        {
+          block = "cpu";
+        }
+        {
+          block = "temperature";
+        }
+        {
+          block = "memory";
+        }
+        {
+          block = "net";
+        }
+        {
+          block = "battery";
+          format = " $icon $percentage $time_remaining ";
+          full_format = " $icon $percentage ";
+          charging_format = " $icon $percentage ";
+        }
+      ];
     };
-    bars.top.blocks = [
-      {
-        block = "time";
-      }
-      {
-        block = "cpu";
-      }
-      {
-        block = "temperature";
-      }
-      {
-        block = "memory";
-      }
-      {
-        block = "net";
-      }
-      {
-        block = "battery";
-        format = " $icon $percentage $time_remaining ";
-        full_format = " $icon $percentage ";
-        charging_format = " $icon $percentage ";
-      }
-    ];
   };
 }
