db52e5f448facd92c9970803e3d992f3ea851ecf
Sam Bartley <SoloJessy.rs@gmail.com>
2026-01-01 10:30:34 +0000

    add cosmic tips and tricks

--- desktopEnvironments/cosmic-base.nix
+++ desktopEnvironments/cosmic-base.nix
@@ -2,6 +2,8 @@
 {
   services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
+  services.system76-scheduler.enable = true;
+  environment.sessionVariables.COSMIC_DATA_CONTROL_ENABLED = 1;
 
   environment.cosmic.excludePackages = with pkgs; [
     cosmic-edit
