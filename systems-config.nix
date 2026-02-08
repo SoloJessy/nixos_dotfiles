4eab5a2a33f587bc47c1863cdbe569e69235167a
Sam Bartley <SoloJessy.rs@gmail.com>
2026-02-08 17:38:49 +0000

    enable A2DP audio sink

--- systems-config.nix
+++ systems-config.nix
@@ -38,6 +38,7 @@
       General = {
         Experimental = true;
         FastConnectable = true;
+        Enable = "Source,Sink,Media,Socket";
       };
       Policy = {
         AutoEnable = true;
