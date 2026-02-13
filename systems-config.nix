030bcbc904da0bfc177441ba579154946b819d99
Sam Bartley <SoloJessy.rs@gmail.com>
2026-02-13 19:44:53 +0000

    remove handset profile for earbuds

--- systems-config.nix
+++ systems-config.nix
@@ -38,7 +38,6 @@
       General = {
         Experimental = true;
         FastConnectable = true;
-        Enable = "Source,Sink,Media,Socket";
       };
       Policy = {
         AutoEnable = true;
@@ -104,6 +103,20 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber = {
+      enable = true;
+      configPackages = [
+        (pkgs.writeTextDir "share/wireplumber.conf.d/51-mitigate-annoying-profile-switch.conf" ''
+          wireplumber.settings = {
+            bluetooth.autoswitch-to-headset-profile = false
+          }
+
+          monitor.bluez.properties = {
+            bluez5.roles = [ a2dp_sink a2dp_source ]
+          }
+        '')
+      ];
+    };
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
